7f86fbaaada7fb4f720520e66e71d03ec915c06c
j-j-brinkmann <j.j.brinkmann@web.de>
2022-09-20 14:37:37 +0200

    implemented routing for the categories

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,21 +1,11 @@
-import CategoryItem from "../category-item/category-item.component";
+import DirectoryItem from "../directory-item/directory-item.component";
 import "./directory.styles.scss";
 
 const Directory = ({ categories }) => {
   return (
     <div className="directory-container">
       {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-        // <div key={category.id} className="category-container">
-        //   <div
-        //     className="background-image"
-        //     style={{ backgroundImage: `url(${category.imageUrl})` }}
-        //   />
-        //   <div className="category-body-container">
-        //     <h2>{category.title}</h2>
-        //     <p>Shop Now</p>
-        //   </div>
-        // </div>
+        <DirectoryItem key={category.id} category={category} />
       ))}
     </div>
   );
---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,32 @@
+import { createContext, useState, useEffect } from "react";
+
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
+
+export const CategoriesContext = createContext({
+  categoriesMap: {},
+});
+
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+
+  // dieser useEffect wurde genutzt um die SHOP_DATA an unsere firebase datenbank zu senden
+  // useEffect(() => {
+  //   addCollectionAndDocuments("categories", SHOP_DATA);
+  // }, []);
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments("categories");
+      setCategoriesMap(categoryMap);
+    };
+
+    getCategoriesMap();
+  }, []);
+
+  const value = { categoriesMap };
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
